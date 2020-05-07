2ec9ec193758ef5ce12aaa5eca0098db0885458e
Brandon Lim <brandonlim.lim@gmail.com>
2020-05-08 09:58:28 +1000

    Support for setting status code and reason phrase for the HTTP response

--- src/HttpResponse.php
+++ src/HttpResponse.php
@@ -9,4 +9,59 @@ namespace HttpClient;
  */
 class HttpResponse extends HttpMessage
 {
+    /**
+     * @var string The response reason phrase associated with the status code.
+     */
+    protected $reasonPhrase;
+
+    /**
+     * @var int The 3-digit integer response status code.
+     */
+    protected $statusCode;
+
+    /**
+     * Return the reason phrase of the response.
+     *
+     * @return string Return the reason phrase.
+     */
+    public function getReasonPhrase()
+    {
+        return $this->reasonPhrase;
+    }
+
+    /**
+     * Set the reason phrase for the response.
+     *
+     * @param string $reasonPhrase The new reason phrase.
+     * @return $this
+     */
+    public function withReasonPhrase($reasonPhrase)
+    {
+        $this->reasonPhrase = $reasonPhrase;
+
+        return $this;
+    }
+
+    /**
+     * Return the status code of the response.
+     *
+     * @return string Return the status code.
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * Set the status code for the response.
+     *
+     * @param string $statusCode The new status code.
+     * @return $this
+     */
+    public function withStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
 }
--- tests/HttpResponseTest.php
+++ tests/HttpResponseTest.php
@@ -18,4 +18,28 @@ class HttpResponseTest extends HttpMessageTest
     {
         return new HttpResponse();
     }
+
+    /**
+     * Test setting reason phrase for a response.
+     */
+    public function testSetReasonPhrase()
+    {
+        $reasonPhrase = 'OK';
+        $response = new HttpResponse();
+        $response->withReasonPhrase($reasonPhrase);
+
+        $this->assertEquals($reasonPhrase, $response->getReasonPhrase());
+    }
+
+    /**
+     * Test setting status code for a response.
+     */
+    public function testSetStatusCode()
+    {
+        $statusCode = 200;
+        $response = new HttpResponse();
+        $response->withStatusCode($statusCode);
+
+        $this->assertEquals($statusCode, $response->getStatusCode());
+    }
 }
